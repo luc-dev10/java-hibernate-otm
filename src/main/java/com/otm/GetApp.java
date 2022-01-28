607ced25e0f54699ac67cbfc29e318fa63bbeba7
luc-dev10 <luc.dev1008@gmail.com>
2022-01-27 19:59:27 -0500

    add get app

--- src/main/java/com/otm/GetApp.java
+++ src/main/java/com/otm/GetApp.java
@@ -1,2 +1,46 @@
-package com.otm;public class GetApp {
+package com.otm;
+
+import com.otm.entity.Course;
+import com.otm.entity.Review;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
+public class GetApp {
+    public static void main(String[] args) {
+        SessionFactory sessionFactory = new Configuration().configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Course.class)
+                .addAnnotatedClass(Review.class)
+                .buildSessionFactory();
+
+        try (Session session = sessionFactory.openSession()) {
+
+            // begin transaction
+            session.beginTransaction();
+
+            // get course
+            int id = 10;
+            Course course = session.get(Course.class, id);
+
+            // get courses
+            System.out.println(course.getTitle());
+
+            // get the reviews
+            List<Review> reviewList = course.getReviewList();
+            reviewList.forEach(review -> {
+                System.out.println(review.getTitle());
+            });
+
+            // commit any transactions
+            session.getTransaction().commit();
+
+            // close
+            session.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
