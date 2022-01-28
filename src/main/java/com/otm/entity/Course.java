48f719d419e51e3f0b5c712b14dea915f0e0a75e
luc-dev10 <luc.dev1008@gmail.com>
2022-01-27 19:56:17 -0500

    add entity classes

--- src/main/java/com/otm/entity/Course.java
+++ src/main/java/com/otm/entity/Course.java
@@ -1,2 +1,58 @@
-package com.otm.entity;public class Course {
+package com.otm.entity;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "course")
+public class Course {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "title")
+    private String title;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+    @JoinColumn(name = "course_id")
+    private List<Review> reviewList;
+
+    public List<Review> getReviewList() {
+        return reviewList;
+    }
+
+    public void setReviewList(List<Review> reviewList) {
+        this.reviewList = reviewList;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void addReview(Review review) {
+
+        // create new list if it does not exist
+        if (this.reviewList == null)
+            reviewList = new ArrayList<>();
+
+        // add if it does not exist
+        if (!reviewList.contains(review))
+            reviewList.add(review);
+
+    }
 }
--- src/main/java/com/otm/entity/Review.java
+++ src/main/java/com/otm/entity/Review.java
@@ -1,2 +1,32 @@
-package com.otm.entity;public class Review {
+package com.otm.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "review")
+public class Review {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "comment")
+    private String title;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
 }
